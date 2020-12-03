88024621d7f604848fd87e10a89f95da6ec95c5d
Bisrat Gebrekidan <github@bisrat.dev>
2020-12-03 16:02:36 -0700

    added better date function integrated new wait function replaced store::visit with vector <int>

--- oreganTrail.cpp
+++ oreganTrail.cpp
@@ -1,6 +1,13 @@
 #include "oreganTrail.hpp"
 #include "usefulFunctions.hpp"
 
+#ifdef _WIN32
+    #include "wait/win.cpp"
+#endif
+#ifdef __unix__
+    #include "wait/uni.cpp"
+#endif
+
 using namespace std;
 string STDEXCEPTIONLOGGER = "Exception caught: ";
 
@@ -26,33 +33,17 @@ int ErrorHandler::validString(string aString){
     }else if(aString.empty()){
         logError("empty string");
         return -2;
-    }else{
-        for(int i = 0; i < 6; i++){
-            if(lowercase(aString) == validYes[i]) return 0;
-        }
-        for(int i = 0; i < 7; i++){
-            if(lowercase(aString) == validNo[i]) return 1;
-        }
-        for(int i = 0; i < 4; i++){
-            if(aString == validExit[i]){
-                logThis("User ended the game abruptly.");
-                exit(EXIT_SUCCESS);
-            }
-        }
-    }
-
-    return 0;
+    }else return 0;
 }
 int ErrorHandler::validStartingDay(int aMonth, int aDay){
     // if the monthy is March or May then the day must be between 1 and 31 days
     if(aMonth == 3 || aMonth == 5){
         if(aDay >= 1 && aDay <= 31) return 0;
-        else if(aMonth == 3) cout << "March has 31 days!\n";
-        else if(aMonth == 5) cout << "May has 31 days!\n";
+        else cout << "Please pick a day between 1 and 31\n";
     // if the month is April, the day must be between 1 and 30
     }else if(aMonth == 4){
         if(aDay >= 1 && aDay <= 30) return 0;
-        else cout << "April has 30 days!\n";
+        else cout << "Please pick a day between 1 and 30\n";
     // if the month is incorrect, then we've had an incorrect month (shouldn't be possible)
     }else{
         logError("invalid month in ErrorHandler::validStartingDay()");
@@ -83,6 +74,10 @@ int ErrorHandler::checkResponse(string sInput){
     if(sInput == "clearlog"){
         clearLog(0);
         return 2;
+    }
+    if(sInput == "cls()"){
+        cls();
+        return -1;
     }
     // check if the response is a valid integer
     if(trySTOI(sInput) != -1){
@@ -214,11 +209,16 @@ int Store::buy(int item, int quantity, int money){
     }
     return 0;
 }
-vector <int> Store::visit(vector <int> aVec){
+double Store::getCost(int index){
+    return costs[index];
+}
+string Store::getItem(int index){
+    return items[index];
+}
+vector <int> Store::visit(vector <int> stats){
     storesVisited++;
     string sInput, numSpaces;
     int iInput;
-    vector <int> stats = aVec;
     oopsStore.setLocation("Store::visit()");
 
     logThis("Player visited "+storeName);
@@ -254,6 +254,7 @@ vector <int> Store::visit(vector <int> aVec){
         }
         cout << "Please input a valid option!\n";
     }
+    return stats;
 }
 
 // Event class
@@ -271,16 +272,6 @@ string lowercase(string input){
     }
     return input;
 };
-/*int wait(float fSeconds) {
-    using namespace std::chrono; // nanoseconds, system_clock, seconds
-    using namespace std::this_thread; // sleep_for, sleep_until
-
-    int iSeconds = fSeconds;
-    int inputToNanoseconds = fSeconds * (10^9);
-    sleep_for(nanoseconds(inputToNanoseconds));
-    sleep_until(system_clock::now() + seconds(iSeconds));
-    return 0;
-}*/
 string centerThis(string dothis, string input){
     // std centering of 88 spaces
     string spaces;
@@ -428,17 +419,13 @@ void initializeLog(){
 void spLoad(){
     for(int i = 0; i < 6; i++){
         cout << '.';
-        // wait(0.5);
+        wait(0.5);
     }
     cout << endl;
     return;
 }
 int restThisTurn(){
     cout << "rested\n";
-    return 0;
-}
-int continueThisTurn(){
-    cout << "onward\n";
     return 0;
 }
 int huntThisTurn(){
@@ -466,58 +453,71 @@ void getName(int index, Traveler aTravelers[]){
         else if(intVR == -2) cout << "Please input a name\n";
         else cout << "That name contains invalid character(s). These include: \"" << invalidChars << "\"\n";
         firstTime = false;
+        cls();
     }
+    cls();
 }
 string getInGameDate(int numdays){
     // 275 days including november 30th
     ErrorHandler oopsGIGD("getInGameDate()");
-    string month, day, year = "1847";
-    int maxdays;
-    if(numdays < 0 || numdays > 31){
+    string month, sDay, year = "1847";
+    int iDay;
+    if(numdays < 1){
         oopsGIGD.logError("invalid numdays");
-        return "error";
+        return "_error";
     }
     if(numdays <= 31){
         month = "March";
-        maxdays = 31;
+        iDay = numdays;
     }else if(numdays <= 61){
         month = "April";
-        maxdays = 30;
+        iDay = numdays - 31;
     }else if(numdays <= 92){
         month = "May";
-        maxdays = 31;
+        iDay = numdays - 61;
     }else if(numdays <= 122){
         month = "June";
-        maxdays = 30;
+        iDay = numdays - 92;
     }else if(numdays <= 153){
         month = "July";
-        maxdays = 31;
+        iDay = numdays - 122;
     }else if(numdays <= 184){
         month = "August";
-        maxdays = 31;
+        iDay = numdays - 153;
     }else if(numdays <= 214){
         month = "September";
-        maxdays = 30;
-    }else if(numdays <= 255){
+        iDay = numdays - 184;
+    }else if(numdays <= 245){
         month = "October";
-        maxdays = 31;
+        iDay = numdays - 214;
     }else if(numdays <= 275){
         month = "November";
-        maxdays = 30;
+        iDay = numdays - 245;
+    }else if(numdays <= 275){
+        month = "December";
+        iDay = numdays - 275;
     }
+
+    sDay = to_string(iDay);
+    if((iDay - 1) % 10 == 0) sDay+="st";
+    else if((iDay - 2) % 10 == 0) sDay+="nd";
+    else if((iDay - 3) % 10 == 0) sDay+="rd";
+    else sDay+="th";
+    
+    return sDay+" of "+month+", "+year;
 }
 int statusUpdate(vector <int> stats, int numdays){
     ErrorHandler oopsStatusUpdate("statusUpdate()");
     string holder = "    ";
     string sSpacesRequired;
     int iSpacesRequired;
-    int spacesRequiredArr[7] = {20,30,20,18,11,10,17};
+    int spacesRequiredArr[8] = {20,32,20,18,11,11,16, 9};
 
     cout << "Here are your current stats:\n";
     centerThis("cout", "It is currently "+getInGameDate(numdays));
-    centerThis("cout", "Miles Traveled:     Distance to Fishman Island:     Food (in lbs.):     Cannon Balls:     Money:     Sails:     Ship Parts:");
+    centerThis("cout", "Miles Traveled:     Distance to Fishman Island:     Food (in lbs.):     Cannon Balls:     Money:     Sails:     Ship Parts:     Med Kits:");
     for(int i = 0; i < 7; i++){
-        iSpacesRequired = spacesRequiredArr[i] - to_string(stats[i+3]).length();
+        iSpacesRequired = spacesRequiredArr[i] - to_string(stats[i]).length();
         for(int i = 0; i < iSpacesRequired; i++){
             sSpacesRequired.push_back(' ');
         }
@@ -533,17 +533,18 @@ int statusUpdate(vector <int> stats, int numdays){
     oopsStatusUpdate.setLocation("Store::unspecified");
     return 0;
 };
-int core(){
+int core(bool debug){
+    srand((unsigned) time(0));
     initializeLog();
 
     Traveler travelers[5];
     Store stores[15];
-    bool visited[15];
-    int intResponse = -1, month, day, numdays = -1;
+    bool visited[15], defaultDate = false;
+    int intResponse = -1, month, day, numdays = 275;
     ErrorHandler oopsCore("core() stats");
     string milestoneNames[8] = {"Shell Town", "Cocoyasi Village", "Syrup Village", "The Baratie", "Drum Island"};
-    string sInput;
     string invalidChars = oopsCore.getInvalidChars();
+    string sInput, crewName;
     // stores miles traveled, miles left to Fishman Island, food, cannon balls, money, sails, ship parts, medical kits
     vector <int> vecStats = {0, 2040, 100, 0, 1600, 0, 0, 0};
 
@@ -554,49 +555,88 @@ int core(){
     centerThis("cout", "Several years ago, the infamous pirate Gold D Roger was captured and put to death by the world governemnt.");
     centerThis("cout", "The world government's goal was to discourage pirates from ever sailing the sea again, but during his execution, Roger would say something that would change the world.");
     centerThis("cout", "\"You want my treausre? You can have it!\". These riviting last words would bring about a new era:");
-    // wait(1.01);
+    wait(1.01);
     centerThis("cout", "~~~ THE GREAT PIRATE ERA! ~~~\n");
-    // wait(0.5);
+    wait(0.5);
     centerThis("cout", "Whether or not he knew it, Roger had just set in motion the greatest events of our lifetime.");
-    // wait(0.5);
+    wait(0.5);
     centerThis("cout", "Touched by Rogers' words, you band together a rag tag group of pirates and set out to sea!");
-    // wait(0.5);
+    wait(0.5);
     centerThis("cout", "Your goal is the new world, but first you must survive");
-    // wait(0.5);
+    wait(0.5);
     centerThis("cout", "~~~ THE GRAND LINE ~~~");
-    // wait(0.5);
+    wait(0.5);
     
+    // get a valid starting month
     oopsCore.setLocation("core() starting date");
     do{
         cout << "You can pick any date between 01, 03, 1847 and 01, 05, 1847 to start your journey\n";
         cout << "In what month will your crew be heading out? > ";
         getline(cin, sInput);
         oopsCore.checkResponse(sInput);
-        if(oopsCore.trySTOI(sInput) == -1){
+
+        if(oopsCore.validString(sInput) == -2){
+            cout << "Default date of 28, 03, 1847 selected\n";
+            month = 3;
+            defaultDate = true;
+            wait(5);
+            cls();
+            break;
+        }else if(oopsCore.trySTOI(sInput) == -1){
             cout << "Please input a valid number\n";
         }else if(oopsCore.validStartingMonth(stoi(sInput)) == -1){
             cout << "Please input a valid month\n";
         }else{
             month = stoi(sInput);
+            wait(5);
+            cls();
             break;
         }
+        cls();
     }while(1);
-
-    do{
+    // get a valid starting day
+    day = 28;
+    while(!defaultDate){
         cout << "On which day will your crew be heading out? > ";
         getline(cin, sInput);
         oopsCore.checkResponse(sInput);
         if(oopsCore.trySTOI(sInput) == -1){
             cout << "Please input a valid number\n";
-        }else if(oopsCore.validStartingDay(month, stoi(sInput) == -1)){
+        }else if(oopsCore.validStartingDay(month, stoi(sInput)) == -1){
             cout << "Please input a valid day\n";
         }else{
             day = stoi(sInput);
+            wait(5);
+            cls();
             break;
         }
-    }while(1);
+        cls();
+    }
+    // convert the starting date to numdays
+    if(month == 3) numdays == day;
+    else if(month == 4) numdays == day + 31;
+    else if(month == 5) numdays == day + 30; 
 
+    //getting names
     cout << "\nPositions and their coresponding names are arbitrary and only serve as a story telling piece\n";
+    while(1){
+        cout << "\nWhat is your crew's name? > ";
+        getline(cin, sInput);
+        intResponse = oopsCore.validString(sInput);
+        if(intResponse == -1){
+            cout << "That name contains invalid strings which include:";
+            for(int i = 0; i < oopsCore.getInvalidChars().length(); i++){
+                cout << "  " << oopsCore.getInvalidChars()[i];
+            }
+            cout << endl;
+        }else if(intResponse == -2) cout << "Please enter a name.\n";
+        else{
+            wait(5);
+            cls();
+            break;
+        }
+        cls();
+    }
     cout << "\nWhat be the Captain's name? (Your name) > ";
     getName(0, travelers);
     cout << "\nWhat is your navigator's name? > ";
@@ -613,34 +653,81 @@ int core(){
     cout << "Time to head out! First thing's first though, lets get some supplies!\n";
     spLoad();
     statusUpdate(vecStats, numdays);
-    //the first store's visit has to be written differently because there are some requirements that not all the stores have
 
-    
+    //the first store's visit has to be written differently because there are some requirements that not all the stores have
+    while(1){
+        centerThis("cout", "Hello captain "+travelers[0].getName()+", let's get you ready for your journey!");
+        stores[0];
+        centerThis("cout", "You and you're crew have managed to save ¥1600");
+        break;
+    }
+    cls();
+
     //turn handling
     oopsCore.setLocation("core() turn handling");
     while(1){
-        string sTurnInput;
-        int iTurnInput;
+        string sTurnInput, turnNumspaces;
+        int iTurnInput, milesTraveled;
+        // check if the user has reached Kansas
+        if(vecStats[1] <= 0){
+            return 0;
+        } 
 
-        cout << "The date is " << getInGameDate(numdays);
-        cout << "What do you want to do?\n[1] Rest\n[2] Hunt\n[3] Continue\n[4] Status Update ";
+
+        // check if the user is out of time
+        if(numdays > 275){ 
+            centerThis("cout", "It is now the "+getInGameDate(numdays));
+            centerThis("cout", "The world government has taken a keen eye to you and your crew.");
+            centerThis("cout", "You took too long getting to the New World at Fishmen Island, and the Navy has caught you\n");
+            centerThis("cout", "Better luck next time!");
+            return 1;
+        }
+        // check if the user has reached any milestones
+
+
+        // what the user wants to do
+        centerThis("cout", "The date is "+getInGameDate(numdays));
+        centerThis("cout", "What do you want to do?");
+        turnNumspaces = centerThis("numspaces", "[1] Rest");
+        cout << turnNumspaces << "[1] Rest\n";
+        cout << turnNumspaces << "[2] Hunt\n";
+        cout << turnNumspaces << "[3] Continue\n";
+        cout << turnNumspaces << "[4] Status Update\n\n";
+        cout << turnNumspaces << "    > ";
         getline(cin, sTurnInput);
         oopsCore.checkResponse(sTurnInput);
 
-        if(sTurnInput == "rest") restThisTurn();
-        else if(sTurnInput == "continue") continueThisTurn();
-        else if(sTurnInput == "hunt") huntThisTurn();
-        else if(sTurnInput == "status update") statusUpdate(vecStats, numdays);
-        else if(oopsCore.trySTOI(sTurnInput) != -1){
-            iTurnInput = stoi(sTurnInput);
-            if(iTurnInput == 1) restThisTurn();
-            else if(iTurnInput == 2) huntThisTurn();
-            else if(iTurnInput == 3) continueThisTurn();
-            else if(iTurnInput == 4) statusUpdate(vecStats, numdays);
-            else cout << "Please input a number 1-4 or their corresponding phrases.\n";
-        }
+        iTurnInput = oopsCore.trySTOI(sTurnInput);
+        if(lowercase(sTurnInput) == "rest" || iTurnInput == 1){// resting takes between 1 and 3 days. Resting consumes 3 lbs. of food, per person, per day. logged
+            numdays+=((rand() % 4));
+            vecStats[2]-=(3*howManyAlive(travelers));
+        }else if(lowercase(sTurnInput) == "hunt" || iTurnInput == 2){// Hunting takes 1 day. logged
+            numdays++;
+        }else if(lowercase(sTurnInput) == "continue" || iTurnInput == 3){// takes 2 weeks. Consumes 3 lbs of food, per person, per day (42 lbs.). Typically travel between 70 and 140 miles per turn. logged
+            numdays+=14;
+            vecStats[2]-=(3*howManyAlive(travelers));
+            milesTraveled = (rand() % 140) + 69;
+        }else if(lowercase(sTurnInput) == "continue" || iTurnInput == 4){
+
+        }else if(iTurnInput == -1 || iTurnInput > 4) cout << "Please input a number 1-4 or their corresponding phrases.\n";
+        else if(debug && sInput == "numdays") cout << " numdays: " << numdays;
         else cout << "Please input \"rest\", \"continue\", \"hunt\", \"status update\", or their corresponding numbers.\n";
+
+        // if(sTurnInput == "rest") restThisTurn();
+        // else if(sTurnInput == "continue") continueThisTurn();
+        // else if(sTurnInput == "hunt") huntThisTurn();
+        // else if(sTurnInput == "status update") statusUpdate(vecStats, numdays);
+        // else if(oopsCore.trySTOI(sTurnInput) != -1){
+        //     iTurnInput = stoi(sTurnInput);
+        //     if(iTurnInput == 1) restThisTurn();
+        //     else if(iTurnInput == 2) huntThisTurn();
+        //     else if(iTurnInput == 3) continueThisTurn();
+        //     else if(iTurnInput == 4) statusUpdate(vecStats, numdays);
+        //     else cout << "Please input a number 1-4 or their corresponding phrases.\n";
+        // }
+        // else cout << "Please input \"rest\", \"continue\", \"hunt\", \"status update\", or their corresponding numbers.\n";
     }
+    cls();
     cout << endl;
     return 0;
 }
