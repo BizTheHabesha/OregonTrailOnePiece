e0453ec213c044cef54fb270deadc5b66b8d710f
Bisrat Gebrekidan <github@bisrat.dev>
2020-12-02 17:15:18 -0700

    added cls definition for windows and unix added return values

--- wait/uni.cpp
+++ wait/uni.cpp
@@ -3,5 +3,10 @@
 int wait(float fseconds){
     unsigned int microsecond = 1000000;
     usleep(fseconds*microsecond);
+    return 0;
 }
-
+void cls(){
+    for(int i = 0; i < 15; i++){
+        cout << endl;
+    }
+}
--- wait/win.cpp
+++ wait/win.cpp
@@ -2,4 +2,8 @@
 // millisecond accuracy
 int wait(float fseconds){
     int milliseconds = fseconds*1000;
+    return 0;W
+}
+void cls(){
+    system("CLS");
 }
